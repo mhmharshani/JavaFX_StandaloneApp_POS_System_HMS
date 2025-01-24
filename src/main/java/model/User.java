e9a4726768dea6267e0a3c48ab83c75a493121dc
mhmharshani <mhmharshani@gmail.com>
2025-01-24 19:18:14 +0530

    First Commit

---
+++ src/main/java/model/User.java
@@ -0,0 +1,19 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class User {
+
+    private String name;
+    private String email;
+
+
+}
